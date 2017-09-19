84bbdfc246cc7d2ce519a2aae23addd0f99366eb
Patrick O'Brien <obrienpja@gmail.com>
2017-09-18 23:21:52 -0400

    Fixes checkIfGroup() function. Makes size-dependent features dynamic.

--- group.cpp
+++ group.cpp
@@ -9,23 +9,28 @@ void Group::cyclicGroup(int dim)
   group.resize(dim, dim);
 
   for(int j = 0; j < dim; j++)
-  {
     for(int i = 0; i < dim; i++)
-    {
       group(i,j) = (i + j) % dim;
-    }
-  }
 }
 
 /**
- * Check if group variable is a group
+ * Check if group variable (which is a multiplication table) is a group
  */
 bool Group::checkIfGroup()
 {
-  int size = 4;
-  for (unsigned j = 0; j < size; j++)
-    for (unsigned k = j + 1; k < size; k++)
-      if (group(j) == group(k)){return false;}
+  int size = sqrt(group.size());
+  for(int i = 0; i < size; i++)
+  {
+    Eigen::MatrixXd groupRow = group.row(i);
+    for(unsigned j = 0; j < size; j++)
+      for(unsigned k = j + 1; k < size; k++)
+        if (groupRow(j) == groupRow(k)){return false;}
+
+    Eigen::MatrixXd groupColumn = group.col(i);
+    for(unsigned j = 0; j < size; j++)
+      for(unsigned k = j + 1; k < size; k++)
+        if (groupColumn(j) == groupColumn(k)){return false;}
+  }
   return true;
 }
 
@@ -35,11 +40,12 @@ bool Group::checkIfGroup()
  */
 Eigen::MatrixXd Group::regularRepresentation(int ele)
 {
-  Eigen::MatrixXd reg(4,4);
-  reg = Eigen::MatrixXd::Zero(4, 4);
+  int size = sqrt(group.size());
+  Eigen::MatrixXd reg(size, size);
+  reg = Eigen::MatrixXd::Zero(size, size);
 
-  for(int i = 0; i < 4; i++)
-    for(int j = 0; j < 4; j++)
+  for(int i = 0; i < size; i++)
+    for(int j = 0; j < size; j++)
       if(group(i, j) == ele) {reg(i, j) = 1;}
 
   std::cout << "The regular representation is: " << std::endl << reg << std::endl << std::endl;
@@ -51,4 +57,5 @@ Eigen::MatrixXd Group::regularRepresentation(int ele)
 void Group::printGroup()
 {
   std::cout << "The group is: " << std::endl << group << std::endl << std::endl;
+  std::cout << "The group size is: " << std::endl << sqrt(group.size()) << std::endl << std::endl;
 }
--- main.cpp
+++ main.cpp
@@ -8,6 +8,13 @@ int main()
   g.cyclicGroup(4);
   g.printGroup();
 
+  std::cout << "The multiplication table is that of a group: " << std::endl;
+
+  if(g.checkIfGroup())
+    std::cout << "True" << std::endl << std::endl;
+  else
+    std::cout << "False" << std::endl << std::endl;
+
   for(int i = 0; i < 4; i++)
   {
     g.regularRepresentation(i);
