dbbb787dfb97296694be974afe01e4087abb8a6f
Shehtab Zaman <shehtabzaman@gmail.com>
2017-09-21 21:47:15 -0400

    Added makefile with commands compile and run. Update EIGEN variable to include eigen root folder

--- group.h
+++ group.h
@@ -2,8 +2,8 @@
 #include <typeinfo>
 #include <complex>
 #include <cmath>
-#include <eigen-eigen-5a0156e40feb/Eigen/Eigenvalues>
-#include <eigen-eigen-5a0156e40feb/unsupported/Eigen/CXX11/Tensor>
+#include <Eigen/Eigenvalues>
+#include <unsupported/Eigen/CXX11/Tensor>
 
 // using namespace std::literals;
 
---
+++ makefile
@@ -0,0 +1,7 @@
+EIGEN = /usr/local/ #Folder Name: eigen-eigen-5a0156e40feb
+
+compile:main.cpp group.cpp
+	g++ main.cpp group.cpp -o main -I $(EIGEN)
+
+run: compile
+	./main
